613c9ac475004e7bdc4f908c38ee780b3e7b2bfa
Craig Wickersham <Wick@Craigs-MacBook-Pro-2.local>
2018-02-20 22:10:49 -0800

    started modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -366,6 +366,25 @@
         </div>
       </footer>
 
+      <!--Modal -->
+
+      <div class="popup">
+        <div class="popup__content">
+            <div class="popup__left">
+              <img src="images/nat-8.jpg" alt="Tour photo" class="popup__img">
+              <img src="images/nat-9.jpg" alt="Tour photo" class="popup__img">
+            </div>
+            <div class="popup__right">
+              <h2 class="heading-secondary u-margin-bottom-small">Start Booking now</h2>
+              <h3 class="heading-tertiary u-margin-bottom-small">Important &ndash; Please read these terms before booking</h3>
+              <p class="popup__text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <a href="#" class="btn btn--green">Book now</a>
+            </div>
+        </div>
+      </div>
+
 </body>
 </html>
 
